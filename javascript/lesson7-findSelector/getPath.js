48d5a785a8d2decfad267302e1987f796f3d07b0
pereverza <d.pereverza@worksolutions.ru>
2019-01-19 19:15:00 +0300

    lesson 7 otus pereverza. fix bugs

--- javascript/lesson7-findSelector/getPath.js
+++ javascript/lesson7-findSelector/getPath.js
@@ -1,13 +1,16 @@
 function getPath(node) {
     const getNext = (node) => {
         const index = Array.from(node.parentNode.children).indexOf(node) + 1;
-        return `${node.nodeName}:nth-child(${index})`;
+        return `${node.nodeName.toLowerCase()}:nth-child(${index})`;
     };
 
     let path = [];
     const getParentId = (node) => {
         if (node.id) {
             return "#" + node.id;
+        }
+        if (!node.parentElement) {
+            return node.nodeName.toLowerCase();
         }
         path.push(getNext(node));
         return getParentId(node.parentElement);
