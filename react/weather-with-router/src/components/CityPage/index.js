9cc2f7c5fb2ba593900724030518ffbfcce9fde5
pereverza <d.pereverza@worksolutions.ru>
2019-02-09 11:17:08 +0300

    lesson 10 otus pereverza. add local storage functional

--- react/weather-with-router/src/components/CityPage/index.js
+++ react/weather-with-router/src/components/CityPage/index.js
@@ -1,10 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class CityPage extends Component {
+import {
+    WeatherAttrs,
+    WeatherCard,
+    WeatherFlow,
+    WeatherTemp,
+    WeatherTitle,
+    WeatherWrapper
+} from "./styled";
+
+export default class CityPage extends React.Component {
+    state = {
+        currentCity: null
+    };
+
+    componentDidMount() {
+        let cacheCity = JSON.parse(localStorage.getItem('favoriteCity'));
+        if (cacheCity) {
+            let currentCity = cacheCity.find(city => city.code === this.props.match.params.code);
+            console.log(currentCity);
+            this.setState({ currentCity: currentCity });
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.state.currentCity !== nextState.currentCity;
+    }
 
     render() {
-        return (
-          <h1>{ this.props.match.params.code }</h1>
+        const { currentCity } = this.state;
+        return currentCity && (
+          <WeatherWrapper>
+              <WeatherCard>
+                  <WeatherTitle>{ currentCity.name }</WeatherTitle>
+                  <WeatherTemp>{ currentCity.data.temperature } `C</WeatherTemp>
+                  <WeatherAttrs>
+                      <span>{ currentCity.data.temperature }</span>
+                      <span>{ currentCity.data.wind }</span>
+                      <span>{ currentCity.data.humidity }</span>
+                  </WeatherAttrs>
+              </WeatherCard>
+              <WeatherFlow>1</WeatherFlow>
+          </WeatherWrapper>
         );
     }
 }
---
+++ react/weather-with-router/src/components/CityPage/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const WeatherWrapper = styled.div`
+  margin: auto;
+  width: 60%;
+  min-height: 50px;
+  background-color: #f3f3f3;
+  border-radius: 10px;
+  padding: 30px;
+`;
+export const WeatherCard = styled.div``;
+export const WeatherTitle = styled.div`
+  font-size: 32px;
+  font-weight: bold;
+ 
+`;
+export const WeatherTemp = styled.div`
+    font-size: 32px;
+`;
+export const WeatherAttrs = styled.div``;
+export const WeatherFlow = styled.div``;
