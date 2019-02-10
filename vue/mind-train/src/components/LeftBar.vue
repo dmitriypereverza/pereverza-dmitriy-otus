b1a43a1c51fb2bb1ca785903bdfd78ef4aa960a5
dmitrijpereverza <disel920@gmail.com>
2019-02-10 23:57:47 +0300

    lesson.11 init vue

---
+++ vue/mind-train/src/components/LeftBar.vue
@@ -0,0 +1,119 @@
+<style scoped>
+  .left {
+    background-color: #7f7f7f;
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+  }
+  .left h1 {
+    font-size: 32px;
+  }
+
+  .left .decsription {
+    font-size: 24px;
+    min-height: 60px;
+  }
+
+  .checkbox-list {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .checkbox-list .checkbox-item {
+    display: flex;
+  }
+
+  .checkbox-list .checkbox-item > input {
+    margin-right: 10px;
+  }
+</style>
+<template>
+  <div class="left">
+    <h1>{{ title }}</h1>
+    <div class="decsription">
+      Добро пожаловать на {{1}} тренировочный день.<br/>
+      Ваш последний результат - решено {{10}} из {{25}}.<br/>
+      Общая точность {{80}}%.
+    </div>
+
+    <h2>Настройки</h2>
+
+    <vue-slider v-model="durationSlider.value" v-bind="durationSlider.options"></vue-slider>
+    <span>Длительность {{durationSlider.value}} минут</span>
+    <vue-slider v-model="complexitySlider.value" v-bind="complexitySlider.options"></vue-slider>
+    <span>Сложность {{complexitySlider.value}}</span>
+
+    <div class="checkbox-list">
+      <div class="checkbox-item">
+        <input type="checkbox" v-model="checkBoxes.checkSums" />
+        <span>Суммирование</span>
+      </div>
+      <div class="checkbox-item">
+        <input type="checkbox" v-model="checkBoxes.checkDifferent" />
+        <span>Разность</span>
+      </div>
+      <div class="checkbox-item">
+        <input type="checkbox" v-model="checkBoxes.checkMultiple" />
+        <span>Умножение</span>
+      </div>
+      <div class="checkbox-item">
+        <input type="checkbox" v-model="checkBoxes.checkSeparation" />
+        <span>Деление</span>
+      </div>
+      <div class="checkbox-item">
+        <input type="checkbox" v-model="checkBoxes.checkPow" />
+        <span>Возведение в степень</span>
+      </div>
+    </div>
+
+    <button @click="submit()">OK</button>
+
+  </div>
+
+</template>
+
+<script>
+  import vueSlider from 'vue-slider-component';
+
+  export default {
+    components: {
+      vueSlider,
+    },
+    props: ['title'],
+    methods: {
+      submit:  function () {
+        console.log(this.checkBoxes);
+      }
+    },
+    data() {
+      return {
+        durationSlider: {
+          value: 1,
+          options: {
+            min: 1,
+            max: 15,
+            tooltip: false,
+            piecewiseLabel: true,
+          },
+        },
+        complexitySlider: {
+          value: 1,
+          options: {
+            min: 1,
+            max: 10,
+            tooltip: false,
+            piecewiseLabel: true,
+          },
+        },
+        checkBoxes: {
+          checkSums: false,
+          checkDifferent: false,
+          checkMultiple: false,
+          checkSeparation: false,
+          checkPow: false,
+        }
+      };
+    },
+  };
+
+</script>
---
+++ vue/mind-train/src/components/RightBar.vue
@@ -0,0 +1,18 @@
+<style scoped>
+  .right {
+    background-color: #7f7f7f;
+    width: 100%;
+    height: 100%;
+  }
+</style>
+<template>
+  <div class="right">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['msg']
+}
+</script>
