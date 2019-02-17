de56a7cd059d410ad0747b0c283eb3b08eea6e32
dmitrijpereverza <disel920@gmail.com>
2019-02-17 17:41:20 +0300

    lesson.11 markup

---
+++ vue/mind-train/src/App.vue
@@ -0,0 +1,27 @@
+<style>
+  #app {
+    display: flex;
+    width: 70%;
+    margin: 60px auto auto;
+  }
+</style>
+
+<template>
+  <div id="app">
+    <LeftBar title="Привет"/>
+    <RightBar msg="Правый"/>
+  </div>
+</template>
+
+<script>
+  import LeftBar from './components/LeftBar.vue';
+  import RightBar from './components/RightBar.vue';
+
+  export default {
+    name: 'app',
+    components: {
+      LeftBar,
+      RightBar,
+    }
+  }
+</script>
