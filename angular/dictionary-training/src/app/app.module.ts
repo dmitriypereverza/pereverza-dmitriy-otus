12caba51cec501273b1688ad7eb1fda3a0f261d9
dmitrijpereverza <disel920@gmail.com>
2019-03-10 21:14:59 +0300

    lesson.14. Структура приложения для запоминания иностранных слов. Добавлен Angular Material

--- angular/dictionary-training/src/app/app.module.ts
+++ angular/dictionary-training/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  MatSliderModule
+} from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecentlyAddedComponent } from './resently-added/recently-added.component';
 import { TrainingComponent } from './training/training.component';
 import { SettingsComponent } from './settings/settings.component';
 import { RouteNotFoundComponent } from './route-not-found/route-not-found.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,9 +24,12 @@ import { RouteNotFoundComponent } from './route-not-found/route-not-found.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Angular Material
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
