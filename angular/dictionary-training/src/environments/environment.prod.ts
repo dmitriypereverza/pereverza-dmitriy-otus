bf333082a8b8d54cf60667373855e8c30cd5d8b6
dmitrijpereverza <disel920@gmail.com>
2019-03-25 21:23:35 +0300

    lesson.14. Структура приложения для запоминания иностранных слов. SSR

--- angular/dictionary-training/src/environments/environment.prod.ts
+++ angular/dictionary-training/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  yaTranslateKey: "trnsl.1.1.20190311T131652Z.2536e29e637d9d85.4a174d1af0541912c9317478d1c510165fd8620d",
+  yaApiUrl: 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 };
